d4b36813db82c501b109a14fb41df5df0e7d1f64
LeonardoLLP <linop3132@gmail.com>
2021-11-19 15:37:40 +0100

    Created initial document

---
+++ blackjack.py
@@ -0,0 +1,15 @@
+cartas = {
+    chr(0x1F0A1): 11,
+    chr(0x1F0A2): 2,
+    chr(0x1F0A3): 3,
+    chr(0x1F0A4): 4,
+    chr(0x1F0A5): 5,
+    chr(0x1F0A6): 6,
+    chr(0x1F0A7): 7,
+    chr(0x1F0A8): 8,
+    chr(0x1F0A9): 9,
+    chr(0x1F0AA): 10,
+    chr(0x1F0AB): 10,
+    chr(0x1F0AD): 10,
+    chr(0x1F0AE): 10,
+}
